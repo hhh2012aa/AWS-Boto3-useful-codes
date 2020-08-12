865e521bbfb0eb15470789c8f5a27cf83d39e462
hhh2012aa <38385178+hhh2012aa@users.noreply.github.com>
2020-08-13 00:08:35 +0800

    Update QuickSight_AutoRefresh.py

--- QuickSight_AutoRefresh.py
+++ QuickSight_AutoRefresh.py
@@ -1,24 +1,26 @@
 import boto3
 import calendar
 import time
+import datetime
+
+
+#-----------------  Input your custom value in "<>" before running -----------
 
 client = boto3.client(
     'quicksight',
-    region_name= <region>
+    region_name= <Region>
 )
 
-for i in client.list_data_sets(AwsAccountId= <YourAWSAccount> )["DataSetSummaries"]:
+for i in client.list_data_sets(AwsAccountId= <AWS ACCOUNT>)["DataSetSummaries"]:
     if i["Name"] == <Dataset Name>:
-        print(i["DataSetId"])
         ID_ = i["DataSetId"]
 
-
-
 while True:
-  ingestionID = calendar.timegm(time.gmtime())
-  response = client.create_ingestion(
-      DataSetId= ID_,
-      IngestionId=str(ingestionID),
-      AwsAccountId=<YourAWSAccount>
-  )
-  time.sleep(<Refresh Interval>)
+    ingestionID = calendar.timegm(time.gmtime())
+    print("Refresh Date:", datetime.datetime.now())
+    response = client.create_ingestion(
+        DataSetId= ID_,
+        IngestionId=str(ingestionID),
+        AwsAccountId= <AWS ACCOUNT>
+    )
+    time.sleep(80)
